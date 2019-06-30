027861c4a2e1ac9564698e0852d997d5e82adf96
Ace <504595380@qq.com>
2019-06-30 21:32:24 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="查询" name="query"><StuQuery></StuQuery></el-tab-pane>
+      <el-tab-pane label="添加" name="add"><StuAdd></StuAdd></el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import TabTest from "./components/TabTest";
+import StuQuery from "./components/StuQuery";
+import StuAdd from "./components/StuAdd";
+export default {
+  name: "app",
+  data() {
+    return {
+      activeName: "query"
+    };
+  },
+  components: {
+    StuAdd,
+    TabTest,
+    StuQuery
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 基本路径
+  publicPath: "",
+  // 输出文件目录
+  outputDir: "docs",
+  // 用于嵌套生成的静态资产（js，css，img，fonts）的目录。
+  assetsDir: "./"
+};
